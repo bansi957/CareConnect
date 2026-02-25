b598190765dc9313a4b8b934572362b98a0c9b05
bansi957 <bansibasha@gmail.com>
2026-02-25 23:32:45 +0530

    backend folders added

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const db = require("./database/mongodb");
 const app = express();
 const port = 3000;
 
 
 app.listen(port, () => {
+    db()
     console.log(`serverv started on port ${port}`)
 })  
