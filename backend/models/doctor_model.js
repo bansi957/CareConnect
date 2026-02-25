4635e89bb38689b26b70851864215b0e3a8da074
Aditya Batchu <batchuaditya369@gmail.com>
2026-02-25 23:45:48 +0530

    add docter schema

---
+++ backend/models/doctor_model.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+
+const doctorSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: String,
+        required: true
+    },
+    pincode: {
+        type: String,
+        required: true
+    },
+    specialization: {
+        type: String,
+        required: true
+    },
+    experience: {
+        type: String,
+        required: true
+    },
+    education: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ["Active", "Inactive"],
+        default: "Active"
+    }
+}, { timestamps: true });
+
+const Doctor = mongoose.model("Doctor", doctorSchema);
+
+module.exports = Doctor;
