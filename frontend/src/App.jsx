cb7dd58a9176e8900334a314fb60c5a7471c28a1
bansi957 <bansibasha@gmail.com>
2026-02-26 17:07:08 +0530

    serverurl added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
 
 import './App.css'
 import Landingpage from './pages/Landingpage'
-import PatientSignUp from './pages/PatientSignUp'
-import { Routes, Route } from 'react-router-dom'
+import PatientSignUp from './pages/PatientSignUp' 
+import HospitalSignUp from './pages/HospitalSignUp'
+  import { Routes, Route } from 'react-router-dom'
+  export const serverUrl="http://localhost:3000"
 function App() {
   return (
     <>
     <Routes>
     <Route path="/" element={<Landingpage />} />
     <Route path="/patient-signup" element={<PatientSignUp />} />
+        <Route path="/hospital-signup" element={<HospitalSignUp />} />
+
     </Routes>
     </>
   );
